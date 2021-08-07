3096e687da46a362b5758d560704cc75838e6c91
fertilewaif <gmkornilov@edu.hse.ru>
2021-08-07 17:59:11 +0300

    add shimmer effect as placeholder for screenshot image

--- app/src/main/java/com/gmkornilov/sberschool/freegames/presentation/features/gameinfo/adapter/ScreenshotAdapter.kt
+++ app/src/main/java/com/gmkornilov/sberschool/freegames/presentation/features/gameinfo/adapter/ScreenshotAdapter.kt
@@ -5,6 +5,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerDrawable
 import com.gmkornilov.sberschool.freegames.R
 import com.gmkornilov.sberschool.freegames.domain.entity.gameinfo.Screenshot
 import com.squareup.picasso.Picasso
@@ -13,10 +15,21 @@ class ScreenshotAdapter : RecyclerView.Adapter<ScreenshotAdapter.ScreenshotViewH
     class ScreenshotViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val image: ImageView = view.findViewById(R.id.screenshotImage)
 
+        private val shimmer = Shimmer.AlphaHighlightBuilder()
+            .setDuration(500)
+            .setBaseAlpha(0.7f)
+            .setHighlightAlpha(0.6f)
+            .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+            .build()
+
+        private val shimmerDrawable = ShimmerDrawable().apply {
+            setShimmer(shimmer)
+        }
+
         fun bind(screenshot: Screenshot) {
             Picasso.get()
                 .load(screenshot.imageUrl)
-                .placeholder(R.drawable.ic_baseline_image_24)
+                .placeholder(shimmerDrawable)
                 .noFade()
                 .fit()
                 .centerCrop()
